fcde09a6a4adb135d388f9bbc04d0255cdb4a1b4
Reda Khouani <reda.khouani@databricks.com>
2024-05-16 23:02:18 +0200

    adding missing semicolons

--- EMR_SQL/Ingest_Incremental.sql
+++ EMR_SQL/Ingest_Incremental.sql
@@ -3,6 +3,6 @@
 -- ProspectIncremental:v_Prospect
 -- FinWire:v_FinWire
 -- BatchDate:v_BatchDate
+
 INSERT INTO {tgt_db}.{table}
-SELECT * FROM {wh_db}_{scale_factor}_stage.{view}
-;
+SELECT * FROM {wh_db}_{scale_factor}_stage.{view};
