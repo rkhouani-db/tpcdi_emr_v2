84f37e3cec6328c1035d60e0450bd5df7bfdd15f
Reda Khouani <reda.khouani@databricks.com>
2024-05-16 12:37:18 +0200

    adding SQL FILES

---
+++ EMR_SQL/Ingest_Incremental.sql
@@ -0,0 +1,7 @@
+-- Databricks notebook source
+-- TABLE:VIEW
+-- ProspectIncremental:v_Prospect
+-- FinWire:v_FinWire
+-- BatchDate:v_BatchDate
+INSERT INTO {tgt_db}.{table}
+SELECT * FROM {wh_db}_{scale_factor}_stage.{view}
