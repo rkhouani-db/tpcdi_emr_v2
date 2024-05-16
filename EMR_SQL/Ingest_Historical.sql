47ce7aac68fd1c86e3714f7fa35019736e6e31ce
Reda Khouani <reda.khouani@databricks.com>
2024-05-17 00:28:10 +0200

    try to fix split func call issue with additional space

--- EMR_SQL/Ingest_Historical.sql
+++ EMR_SQL/Ingest_Historical.sql
@@ -2,8 +2,7 @@ INSERT INTO {wh_db}_{scale_factor}.{table}
 SELECT {tgt_query}
 FROM
   (
-    SELECT
-      split(value, "[|]") val
+    SELECT split(value, "[|]") val 
     FROM
       text.`{tpcdi_directory}sf={scale_factor}/{path}/{filename}`
   );
